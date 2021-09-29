c71e15140679c510417a242197a4dcfb2caac63c
T P <15642007+RoBlockHead@users.noreply.github.com>
2021-09-29 22:59:37 +0000

    potentially fix postcss issue

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -7,9 +7,7 @@ const dev = mode === "development";
 
 const config = {
 	plugins: [
-        //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
         tailwindcss(),
-        //But others, like autoprefixer, need to run after,
         autoprefixer(),
         !dev && cssnano({
 			preset: "default",
