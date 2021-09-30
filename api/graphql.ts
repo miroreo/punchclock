44b918275c6f3fc5f6b83f2358834adc0b7b1937
T P <15642007+RoBlockHead@users.noreply.github.com>
2021-09-30 02:57:20 +0000

    use the example code maybe works?

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,46 +1,54 @@
-import { ApolloServer } from "apollo-server-micro";
+import { ApolloServer, gql } from "apollo-server-micro";
 import { getConnection } from "../src/database";
-import { gql } from "apollo-server-micro";
+// import {  } from "apollo-server-micro";
 
 // import noteSchema from "./note";
 // import customSchema from "./custom";
 
-const linkSchema = gql`
+// const linkSchema = gql`
+//   type Query {
+// 		getHello: String!
+//   }
+// `;
+
+// // import typeDefs from "../src/graphql/schema";
+// // import resolvers from "../src/graphql/resolvers";
+
+// const apolloServer = new ApolloServer({
+//   typeDefs: linkSchema,
+//   resolvers: {
+// 		// Date: GraphQLDate,
+// 		// Time: GraphQLTime,
+// 		// DateTime: GraphQLDateTime,
+// 		Query: {
+// 			getHello: async (): Promise<String> => {
+// 				return new Promise((res) => "Hello World!");
+// 			},
+// 		}
+// 	},
+//   // context: async () => {
+//   //   const dbConn = await getConnection();
+
+//   //   return { dbConn };
+//   // },
+//   // introspection: true
+// });
+
+
+const typeDefs = gql`
   type Query {
-    _: Boolean
-		getHello: String!
-  }
-
-  type Mutation {
-    _: Boolean
-  }
-
-  type Subscription {
-    _: Boolean
+    sayHello: String
   }
 `;
 
-// import typeDefs from "../src/graphql/schema";
-// import resolvers from "../src/graphql/resolvers";
+const resolvers = {
+  Query: {
+    sayHello(parent, args, context) {
+      return 'Hello World!';
+    },
+  },
+};
 
-const apolloServer = new ApolloServer({
-  typeDefs: linkSchema,
-  resolvers: {
-		// Date: GraphQLDate,
-		// Time: GraphQLTime,
-		// DateTime: GraphQLDateTime,
-		Query: {
-			getHello: async (): Promise<String> => {
-				return new Promise((res) => "Hello World!");
-			},
-		}
-	},
-  // context: async () => {
-  //   const dbConn = await getConnection();
-
-  //   return { dbConn };
-  // },
-  introspection: true
-});
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 export default apolloServer.createHandler({ path: "/api/graphql" });
