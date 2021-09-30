4a13e0b15e7a20266beb34400d5f28e0b9ddeabf
T P <15642007+RoBlockHead@users.noreply.github.com>
2021-09-30 02:22:57 +0000

    aaaaaaa

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,11 +1,30 @@
 import { ApolloServer } from "apollo-server-micro";
 import { getConnection } from "../src/database";
+import { gql } from "apollo-server-micro";
+
+// import noteSchema from "./note";
+import customSchema from "./custom";
+
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+		getHello: String!
+  }
+
+  type Mutation {
+    _: Boolean
+  }
+
+  type Subscription {
+    _: Boolean
+  }
+`;
 
 import typeDefs from "../src/graphql/schema";
 import resolvers from "../src/graphql/resolvers";
 
 const apolloServer = new ApolloServer({
-  typeDefs,
+  typeDefs: linkSchema,
   resolvers,
   context: async () => {
     const dbConn = await getConnection();
